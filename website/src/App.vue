2c20dee5ed026a143d15bd5a95f9aa406b670201
estridha <estridhassbring@gmail.com>
2023-02-14 15:42:15 +0100

    added header component

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <AppHeader/>
   <router-view/>
 </template>
 
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 #app {
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ website/src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/experience">Experience</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+nav {
+  display: flex;
+  position: sticky;
+  top: 0;
+  justify-content: center;
+  width: 100%;
+  padding: 30px;
+
+  a {
+    padding: 0 2rem;
+    font-weight: bold;
+    color: #000000;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #ffa321;
+    }
+  }
+}
+</style>
