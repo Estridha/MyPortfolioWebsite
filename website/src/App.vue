bf11563e5475069904fa3ba50a01dbf9f46aba6e
estridha <estridhassbring@gmail.com>
2023-02-19 21:12:53 +0100

    added css variables

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="wrapper" :style="{height: (windowHeight.toString() + 'px')}">
+  <div class="wrapper" :style="{ height: windowHeight.toString() + 'px' }">
     <AppHeader />
     <div class="content-container">
-    <router-view />
-    <AppFooter />
-  </div>
+      <router-view />
+      <AppFooter />
+    </div>
   </div>
 </template>
 
@@ -20,44 +20,63 @@ export default {
   },
   data() {
     return {
-      windowHeight: window.innerHeight
-    }
+      windowHeight: window.innerHeight,
+    };
   },
   mounted() {
     this.$nextTick(() => {
-      window.addEventListener('resize', this.onResize);
-    })
+      window.addEventListener("resize", this.onResize);
+    });
   },
-  beforeUnmount() { 
-    window.removeEventListener('resize', this.onResize); 
+  beforeUnmount() {
+    window.removeEventListener("resize", this.onResize);
   },
-  methods: {  
+  methods: {
     onResize() {
-      this.windowHeight = window.innerHeight
-    }
-  }
+      this.windowHeight = window.innerHeight;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap');
+@import './assets/styles/variables.css';
+
 * {
   box-sizing: border-box;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: 'Inter', sans-serif;
   list-style: none;
   text-decoration: none;
 }
 html,
 body {
   margin: 0;
+  background-color: var(--light-color);
+  color: var(--dark-color);
+  h1,
+  h2,
+  h3 {
+    font-weight: 800;
+  }
+  h4 {
+    font-weight: 600;
+  }
+  p,
+  a,
+  label, button {
+    font-weight: 400;
+  }
+  a {
+    color: var(--link-color);
+  }
 }
 
 #app {
   margin: 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   overflow: none;
 }
 
